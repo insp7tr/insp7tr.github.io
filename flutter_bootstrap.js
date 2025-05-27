68a680cfec77eec1cf13d19916a677bd4ca278ed
TALHA PATEL <talha.patel@toppansecurity.com>
2025-05-27 15:08:47 +0200

    Added Validate Photograph Tester

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4188316587"
+    serviceWorkerVersion: "2247749351"
   }
 });
